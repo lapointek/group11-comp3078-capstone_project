f1bca6b5689493ddde5bcbfb2a66ea2faa013bb7
lapointek <156107790+lapointek@users.noreply.github.com>
2025-02-22 17:45:42 -0500

    modified Login.jsx with tailwind css and a handleSubmit function

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,7 +1,54 @@
 import React from "react";
 
 const Login = () => {
-  return <div>Login</div>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault;
+  };
+
+  return (
+    <div>
+      <div
+        className="flex flex-col items-center h-screen 
+      justify-center bg-gradient-to-b from-sky-600 from-50% to-50% space-y-6"
+      >
+        <h2 className="font-sevillana text-3xl text-white">
+          Employee Managment
+        </h2>
+        <div className="border shadow p-6 w-80 bg-white">
+          <h2 className="text-2xl font-bold mb-4">Login</h2>
+          <form onSubmit={handleSubmit}>
+            <div className="mb-4">
+              <label htmlFor="email" className="block text-gray-700">
+                Email
+              </label>
+              <input
+                type="email"
+                className="w-full px-3 py-2 border"
+                placeholder="Enter Email"
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </div>
+            <div className="mb-4">
+              <label htmlFor="password" className="block text-gray-700">
+                Password
+              </label>
+              <input
+                type="password"
+                className="w-full px-3 py-2 border"
+                placeholder="********"
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </div>
+            <div className="mb-4"></div>
+            <button className="w-full bg-sky-600 text-white py-2">Login</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
