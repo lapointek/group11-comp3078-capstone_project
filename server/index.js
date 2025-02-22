d56f65cd89d65a6f1efcc690527bdb1c60a277aa
lapointek <156107790+lapointek@users.noreply.github.com>
2025-02-22 16:05:15 -0500

    added User.js model, added userSeed.js to add an admin user, changed db.js path to be inside of the db folder, modified index.js and the db.js path

--- server/index.js
+++ server/index.js
@@ -1,19 +1,19 @@
-import express from "express"
-import cors from "cors"
-import {connectDB} from "./db.js"
+import express from "express";
+import cors from "cors";
+import { connectDB } from "./db/db.js";
 
-const app = express()
-app.use(cors())
-app.use(express.json())
+const app = express();
+app.use(cors());
+app.use(express.json());
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 
 connectDB()
-.then(() => {
-    app.listen(PORT, () =>{
-        console.log(`Server successfully connected at ${PORT}`)
-    })
-})
-.catch((err) => {
-    console.error("Failed to connect to database: ", err)
-})
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server successfully connected at port ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error("Failed to connect to database: ", err);
+  });
