4f0b2ea82b5d29bf3659d30a7a0a610373c2b654
Kevin Lapointe <156107790+lapointek@users.noreply.github.com>
2025-03-07 14:02:45 -0500

    fixed connection to mongodb database

--- server/index.js
+++ server/index.js
@@ -1,8 +1,10 @@
+import dotenv from "dotenv";
 import express from "express";
 import cors from "cors";
 import connectDB from "./db/db.js";
 import authRouter from "./routes/auth.js";
 
+dotenv.config();
 const app = express();
 app.use(cors());
 app.use(express.json());
@@ -11,11 +13,11 @@ app.use("/api/auth", authRouter);
 const PORT = process.env.PORT || 5000;
 
 connectDB()
-  .then(() => {
-    app.listen(PORT, () => {
-      console.log(`Server successfully connected at port ${PORT}`);
+    .then(() => {
+        app.listen(PORT, () => {
+            console.log(`Server successfully connected at port ${PORT}`);
+        });
+    })
+    .catch((err) => {
+        console.error("Failed to connect to database: ", err);
     });
-  })
-  .catch((err) => {
-    console.error("Failed to connect to database: ", err);
-  });
