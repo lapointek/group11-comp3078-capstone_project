6a86ed8a8b89eb5d47d48e57b605716ab0665067
lapointek <156107790+lapointek@users.noreply.github.com>
2025-03-25 19:32:12 -0400

    useEffect function in authContext.jsx for verification

--- frontend/src/context/authContext.jsx
+++ frontend/src/context/authContext.jsx
@@ -6,6 +6,38 @@ const userContext = createContext();
 const authContext = ({ children }) => {
   const [user, setUser] = useState(null);
 
+  const navigate = useNavigate();
+
+  // verify user
+  useEffect(() => {
+    const verifiedUser = async () => {
+      try {
+        const token = localStorage.getItem("token");
+        if (token) {
+          const response = await axios.get(
+            "http://localhost:3000/api/auth/verify",
+            {
+              // pass authorization token
+              headers: {
+                Authorization: `Bearer ${token}`,
+              },
+            }
+          );
+          if (response.data.success) {
+            setUser(response.data.user);
+          }
+        } else {
+          setUser(null);
+        }
+      } catch (error) {
+        if (error.reponse && !error.response.data.error) {
+          setUser(null);
+        }
+      }
+    };
+    verifyUser();
+  }, []);
+
   // login user - passing user from Login
   const login = (user) => {
     setUser(user);
--- frontend/src/pages/AdminDashboard.jsx
+++ frontend/src/pages/AdminDashboard.jsx
@@ -4,7 +4,7 @@ import { useAuth } from "../context/authContext";
 const AdminDashboard = () => {
   const { user } = useAuth();
   // print username
-  return <div>Admin Dashboard {user.name}</div>;
+  return <div>Admin Dashboard {user && user.name}</div>;
 };
 
 export default AdminDashboard;
