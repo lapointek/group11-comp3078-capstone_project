73da8fdb6d6ef049021932a211d6e8a1e28f2f24
lapointek <156107790+lapointek@users.noreply.github.com>
2025-03-26 10:35:24 -0400

    added top bar styling and button for logout in Navbar.jsx and AdminDashboard.jsx

--- frontend/src/pages/AdminDashboard.jsx
+++ frontend/src/pages/AdminDashboard.jsx
@@ -1,14 +1,18 @@
 import React from "react";
 import { useAuth } from "../context/authContext";
 import AdminSidebar from "../components/dashboard/AdminSidebar";
+import Navbar from "../components/dashboard/Navbar";
 
 const AdminDashboard = () => {
   const { user } = useAuth();
 
   // if loading is true
   return (
-    <div>
+    <div className="flex">
       <AdminSidebar />
+      <div className="flex-1 ml-64 bg-gray-100 h-screen">
+        <Navbar />
+      </div>
     </div>
   );
 };
